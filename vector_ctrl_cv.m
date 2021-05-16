040835e5790921a8b78e73056196a86d0b78c5e8
keesung98 <keesung98@naver.com>
2021-05-16 18:23:50 +0900

    Create vector_ctrl_cv.m

---
+++ vector_ctrl_cv.m
@@ -0,0 +1,17 @@
+clc
+clear
+
+x=[1 2 3];
+n=input("정수입력 : ");
+y=duplicate_n(x,n);
+y
+
+function y=duplicate_n( x, n )
+new=[];
+for i=1:3
+    for j=1:n
+        new=[new x(i)];
+    end
+end
+y=new;
+end
