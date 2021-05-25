1a2d9fbb43f35d6cbed45c7ea9f88c1173b715e9
keesung98 <keesung98@naver.com>
2021-05-25 18:38:17 +0900

    Update four_sqr.m

--- four_sqr.m
+++ four_sqr.m
@@ -1,16 +1,21 @@
 clc
 clear
 hold on
+
 x=[-1,-1,1,1,-1];
 y=[-1,1,1,-1,-1];
 plot(x,y,'c','LineWidth',2)
+
 x=[0,2,0,-2,0];
 y=[2,0,-2,0,2];
 plot(x,y,'k','LineWidth',2)
+
 x=[-2,-2,2,2,-2];
 y=[-2,2,2,-2,-2];
 plot(x,y,'b','LineWidth',2)
+
 x=[0,4,0,-4,0];
 y=[4,0,-4,0,4];
 plot(x,y,'y','LineWidth',2)
+
 grid on
