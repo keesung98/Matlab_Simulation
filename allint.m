3bd523cc622b15e900ca5210c5d67c06f5b07c7b
keesung98 <keesung98@naver.com>
2021-05-29 20:37:50 +0900

    Create allint.m

---
+++ allint.m
@@ -0,0 +1,23 @@
+clc
+clear
+
+format compact;
+x = [1 2 3 4]
+a = 0
+b = 5
+B = allint(x, a, b)
+function y=allint(x,a,b)
+key=0;
+for i=1:length(x)
+    if x(i)<a
+        key=1;
+    elseif x(i)>b
+        key=1;
+    end
+end
+if key==1
+    y=false;
+else
+    y=true;
+end
+end
