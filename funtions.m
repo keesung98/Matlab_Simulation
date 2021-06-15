899d633889ee52525c4edf5e55fd3751dfd76217
keesung98 <keesung98@naver.com>
2021-06-15 16:28:47 +0900

    Create funtions.m

---
+++ funtions.m
@@ -0,0 +1,27 @@
+%clc 초기화
+%clear 명령창 삭제
+%fprintf  출력
+%input 입력
+%floor 내림
+%abs 절대값
+%while ()
+%for k=1:n   k는 1부터 n까지 1씩증가
+%zeros(1,5)=(0,0,0,0,0)
+%magic(a)  a*a마방진
+%x=1:5     (1,2,3,4,5)
+%linspace(a,b,n) a부터 b까지 n개의 점으로 표현
+%exp 지수 e
+%plot 그림그리기 xlabel('x') x축 이름 ylabel('y') y축 이름 title('함수fx') 함수 이름
+%fill(x,y,v)   x좌표 y좌표 v색깔
+%axis 축제한
+%function y=f(x)  함수
+%rand(n) 0부터 1사이의 균일 분포 난수 생성
+%randn(n) 정규분포 난수생성
+%mod(a,b) 나머지 연산자 a를 b로 나눈 나머지 구하기
+%a=[a b]  a벡터 뒤에 b를 붙임
+%length(a) a의 원소의 갯수
+%A(행ㅡ;열I)
+%fprintf("%8.2f",x)  8칸을 배열을 만들고 소수점 2째자리까지 출력
+%[a,b]=meshgrid(x,y)  x,y에 저장된 좌표를 바탕으로 2차원 행열생성
+%contour(x,y,Z)  x,y축을 바탕으로 z등고선을 표시 출력
+
